cc7e124729d228cb6e039b7c55d834fbd02a806a
suy1214-spec <suy1214@gachon.ac.kr>
2025-11-12 01:17:55 +0900

    submit Lab 11 package

---
+++ tester.py
@@ -0,0 +1,10 @@
+
+import geo.utils as utils 
+
+a, b = 3, 4
+c = utils.pythagoras(a, b)
+print(f"피타고라스 (a=3, b=4) -> c = {c}")
+
+r = 10
+area = utils.circle(r) 
+print(f"원 넓이 (r=10) -> area = {area}")
